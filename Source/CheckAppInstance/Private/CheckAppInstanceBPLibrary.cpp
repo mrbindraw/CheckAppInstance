35cb938f92307ea9bc579a4f846544ad7ea497c9
Andrew Bindraw <mrbindraw@gmail.com>
2020-09-22 12:48:20 +0300

    update copyright in source code

--- Source/CheckAppInstance/Private/CheckAppInstanceBPLibrary.cpp
+++ Source/CheckAppInstance/Private/CheckAppInstanceBPLibrary.cpp
@@ -1,4 +1,4 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright 2020 Andrew Bindraw. All Rights Reserved.
 
 #include "CheckAppInstanceBPLibrary.h"
 #include "CheckAppInstance.h"
